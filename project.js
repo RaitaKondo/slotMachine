3475cbb63185e365f57623c4d3a06856dfb190f6
Raita Kondo <raycondo915@gmail.com>
2023-12-06 00:13:27 +0700

    first commit

---
+++ project.js
@@ -0,0 +1,7 @@
+//collect the deposit money
+//determine number of the lines to bet on
+//collect a bet amount
+//spin the slot machine
+//check if the user win
+// give the user their winnings
+// play again
